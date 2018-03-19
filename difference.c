25428ad1d38616e8b0b0c823a7ca58fc7f05714a
santhiyasanthra <31505577+santhiyasanthra@users.noreply.github.com>
2018-03-19 10:36:05 +0000

    Create difference.c

---
+++ difference.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	int kabali,opponent,difference;
+	scanf("%d",&kabali);
+	scanf("%d",&opponent);
+	difference=opponent-kabali;
+	printf("%d",difference);
+	return 0;
+}
